930d4f5c3a945e36894ee684da652eb9ec8d1a2e
L0Wigh <mathiotthomas@gmail.com>
2021-10-24 15:43:52 +0200

    Version 0.0.1 Final

--- lionvim/lua/plugins/dashboard.lua
+++ lionvim/lua/plugins/dashboard.lua
@@ -16,7 +16,7 @@ vim.g.dashboard_custom_header = {
 "     AMMMMM  ,        ~~~~~,aooooa,~~~~~~      MMMMMMMMMMM",
 "       YMMMb,d'         dMMMMMMMMMMMMMD,   a,, AMMMMMMMMMM",
 "                                                          ",
-"                                         LionVim 0.0.1-DEV"
+"                                             LionVim 0.0.1"
 }
 
 local custom_section = {
